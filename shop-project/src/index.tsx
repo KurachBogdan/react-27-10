1f73a3ce91083fa403e4d049e9231105a54a3483
Bohdan Kurach <kurach.link@gmail.com>
2023-01-30 09:53:17 +0200

    add jsx rules

--- shop-project/src/index.tsx
+++ shop-project/src/index.tsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const h1 = <h1 id="title">Hello, React.js</h1>
+const h1 = (
+    <h1 id="title" className="edit">
+        Hello, React.js
+    </h1>
+)
+
+let a = 10
+let b = 'Hello, React'
 
 const list = (
     <ul>
-        <li>List Item 1</li>
-        <li>List Item 2</li>
+        <li>List Item 1 {a + 10 + 10}</li>
+        <li>List Item 2 {b}</li>
         <li>List Item 3</li>
     </ul>
 )
-
 
 const content = (
     <div>
