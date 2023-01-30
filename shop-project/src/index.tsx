a4a1922dbad77c8c2a63abfecde1acca8f10fca1
Bohdan Kurach <kurach.link@gmail.com>
2023-01-30 09:43:48 +0200

    create ReactElement with jsx syntax

--- shop-project/src/index.tsx
+++ shop-project/src/index.tsx
@@ -1,5 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+const h1 = <h1 id="title">Hello, React.js</h1>
+
+const list = (
+    <ul>
+        <li>List Item 1</li>
+        <li>List Item 2</li>
+        <li>List Item 3</li>
+    </ul>
+)
+
+
+const content = (
+    <div>
+        {h1}
+        {list}
+    </div>
+)
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode></React.StrictMode>)
+root.render(<React.StrictMode>{content}</React.StrictMode>)
