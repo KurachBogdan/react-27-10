f600c98f07ee93e1ff033ede03813af6eae77e63
Bohdan Kurach <kurach.link@gmail.com>
2023-01-31 17:42:04 +0200

    removed files and added absolut imports

--- shop-project/src/index.tsx
+++ shop-project/src/index.tsx
@@ -1,25 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Content } from './Content'
-import Title from './Title'
-
-function App() {
-    return (
-        <>
-            <Title text="React" />
-            <Title text="TS" />
-            <Content
-                text1="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus eaque optio dolor necessitatibus, officia doloremque, sed maxime inventore neque dolores ullam asperiores."
-                text2="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus eaque optio dolor necessitatibus, officia doloremque, sed maxime inventore neque dolores ullam asperiores."
-                year={2023}
-            />
-        </>
-    )
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-)
+root.render(<React.StrictMode></React.StrictMode>)
