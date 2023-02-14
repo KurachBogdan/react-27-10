dd68fd1a1640ed6e9273a07982f3f711e562a2d7
Bohdan Kurach <kurach.link@gmail.com>
2023-02-14 19:23:10 +0200

    added useState to change the count

--- shop-project/src/components/ProductsList/ProductsListItem.tsx
+++ shop-project/src/components/ProductsList/ProductsListItem.tsx
@@ -1,4 +1,11 @@
-import { Button, Card, CardActions, CardContent, TextField } from '@mui/material'
+import {
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    TextField,
+} from '@mui/material'
+import { useState } from 'react'
 import './ProductsListItem.scss'
 
 type Props = {
@@ -18,6 +25,16 @@ const ProductsListItem = ({
     price,
     image,
 }: Props) => {
+    const [count, setCount] = useState<number>(0)
+
+    const onIncrement = () => {
+        setCount(count + 1)
+    }
+
+    const onDecrement = () => {
+        setCount(count - 1)
+    }
+
     return (
         <Card variant="outlined" className="product">
             <CardContent>
@@ -30,13 +47,25 @@ const ProductsListItem = ({
                 <div className="product-features">Capacity: {capacity} Gb</div>
                 <div className="product-price">{price} $</div>
                 <div className="product-quantity">
-                    <Button variant="outlined">-</Button>
-                    <TextField size="small" value="1" />
-                    <Button variant="outlined">+</Button>
+                    <Button
+                        variant="outlined"
+                        onClick={() => onDecrement()}
+                    >
+                        -
+                    </Button>
+                    <TextField size="small" value={count} />
+                    <Button
+                        variant="outlined"
+                        onClick={() => onIncrement()}
+                    >
+                        +
+                    </Button>
                 </div>
             </CardContent>
             <CardActions className="product-btn-wrap">
-                <Button className='cart' variant="outlined">Add to cart</Button>
+                <Button className="cart" variant="outlined">
+                    Add to cart
+                </Button>
             </CardActions>
         </Card>
     )
