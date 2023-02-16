00394e25c9eff8f13b574275fb219d04eae0fc95
Bohdan Kurach <kurach.link@gmail.com>
2023-02-16 10:19:42 +0200

    Прописав шлях від вибору товару з його ціною до корзини

--- shop-project/src/container/Header/Header.tsx
+++ shop-project/src/container/Header/Header.tsx
@@ -8,8 +8,14 @@ import Menu from 'components/Menu/Menu'
 import './Header.scss'
 import CartHeader from 'components/CartHeader/CartHeader'
 
-type Props = {}
-const Header = (props: Props) => {
+type Props = {
+    cartData: {
+        totalCount: number
+        totalPrice: number
+    }
+}
+
+const Header = ({ cartData }: Props) => {
     return (
         <AppBar position="static" className="app-bar">
             <Container maxWidth="lg">
@@ -25,7 +31,7 @@ const Header = (props: Props) => {
                     </IconButton>
                     <Logo />
                     <Menu />
-                    <CartHeader />
+                    <CartHeader cartData={cartData} />
                 </Toolbar>
             </Container>
         </AppBar>
