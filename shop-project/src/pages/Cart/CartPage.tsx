41f9ddb6c7b1f491b96c96dac8f29763c00c7971
Bohdan Kurach <kurach.link@gmail.com>
2023-02-28 00:41:43 +0200

    added CartProductListItem and Extended

--- shop-project/src/pages/Cart/CartPage.tsx
+++ shop-project/src/pages/Cart/CartPage.tsx
@@ -1,23 +1,32 @@
+import { Grid, Typography } from '@mui/material'
 import CartProductList from 'components/CartProductList/CartProductList'
+import CartProductListItemExtended from 'components/CartProductList/CartProductListItemExtended'
 import CartTotal from 'components/CartTotal/CartTotal'
-import productsArray, { getProductsObject, Product } from 'utils/productsArray'
 
 type Props = {
     productsInCart: {
         [id: number]: number
     }
-    productsObject?: {
-        [id: number]: Product
-    }
 }
 
-const CartPage = ({
-    productsInCart,
-    productsObject = getProductsObject(productsArray),
-}: Props) => {
+const CartPage = ({ productsInCart }: Props) => {
     return (
         <div>
-            <CartProductList productsInCart={productsInCart} />
+            <Typography
+                variant="h4"
+                component="h1"
+                sx={{
+                    marginBottom: '30px',
+                }}
+            >
+                Cart
+            </Typography>
+            <Grid container spacing={4}>
+                <CartProductList
+                    productsInCart={productsInCart}
+                    CartItem={CartProductListItemExtended}
+                />
+            </Grid>
             <CartTotal productsInCart={productsInCart} />
         </div>
     )
